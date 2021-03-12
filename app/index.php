8179987d0bdd9f1123d87c3b61e930dba4f22e62
Anthony Bible <anthony@anthony.bible>
2021-03-11 20:55:40 -0700

    Changed site captcha key

--- app/index.php
+++ app/index.php
@@ -69,7 +69,7 @@
             <div id="success"></div>
                         <div class="row">
                             <div  class="form-group col-xs-12">
-                            <div class="g-recaptcha" data-sitekey="6LcFT00aAAAAAC5bafMxkRnAF-WuoBPXKQMK5Pho"></div>
+                            <div class="g-recaptcha" data-sitekey="6LeGDHwaAAAAADAGSax5zzuM16NPJmjKOX5jOgP9"></div>
                                 <button id="contactFormSubmit"   type="submit" class="btn btn-success btn-lg">Send</button>
                             <div id="wasitasuccess"> </div>
                             </div>
