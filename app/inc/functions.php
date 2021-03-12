c70faaaf31b27579d4229890aec9c3c2c4235978
Anthony Bible <anthony@anthony.bible>
2021-03-12 07:40:55 -0700

    Spelling

--- app/inc/functions.php
+++ app/inc/functions.php
@@ -29,40 +29,40 @@ function RandomToken($length = 32){
         return bin2hex(openssl_random_pseudo_bytes($length));
     }
 }
-function EcryptMessage($messagedata, $encryption_key){
+function EncryptMessage($messagedata, $encryption_key){
     $ciphering = "AES-128-CTR"; 
     $iv = openssl_random_pseudo_bytes(openssl_cipher_iv_length($cypherMethod));
     $encryption = openssl_encrypt($messagedata, $ciphering, 
     $encryption_key, $options=0, $encryption_iv); 
     return array($iv, $encyrption, $encryption_key);
 }
-function storeMessageinDatabase($encryptedstring, $firstname, $lastname)
-{
-    $link=$db->connect();
-    if (!($encryptedstring==''|| $firstname==''||$lastname='')){
-        $sql = "INSERT INTO messages (encryptedstring, firstname, lastname) VALUES (:encryptedstring, :firstname, :lastname )";
+// function storeMessageinDatabase($encryptedstring, $firstname, $lastname)
+// {
+//     $link=$db->connect();
+//     if (!($encryptedstring==''|| $firstname==''||$lastname='')){
+//         $sql = "INSERT INTO messages (encryptedstring, firstname, lastname) VALUES (:encryptedstring, :firstname, :lastname )";
           
-          $sqlData  = array(
-                    ":user"=>$_SESSION['user_id'],
-                    ":content"=>$content,
-                    ":likes"=> "0", 
-                    ":retweets" => "0",
-                    );
-           echo "<errors>".
-                " No Errors".
-                "</errors>";     
-          $stmt = $link->prepare($sql);
-          $stmt->execute($sqlData);
+//           $sqlData  = array(
+//                     ":user"=>$_SESSION['user_id'],
+//                     ":content"=>$content,
+//                     ":likes"=> "0", 
+//                     ":retweets" => "0",
+//                     );
+//            echo "<errors>".
+//                 " No Errors".
+//                 "</errors>";     
+//           $stmt = $link->prepare($sql);
+//           $stmt->execute($sqlData);
 
-  }else{
-       echo "<errors>".
-            "You need to enter something in the content area".
-            "</errors>"; 
+//   }else{
+//        echo "<errors>".
+//             "You need to enter something in the content area".
+//             "</errors>"; 
 
-  }
-echo "</response>";
+//   }
+// echo "</response>";
 
-}
+// }
 
 function mailVerify($usertoken,$firstname,$email){
 	$secondEmail=urlencode($email);
