7fc34782eb35a1a0cf35b2dd1cc495abe62ded77
Anthony Bible <anthony@anthony.bible>
2021-06-13 16:56:07 -0600

    start encryption fuctions

--- go-rewrite/encryption.go
+++ go-rewrite/encryption.go
@@ -2,8 +2,12 @@ package main
 
 import (
     "crypto/rand"
+    "crypto/aes"
     "encoding/base64"
     "github.com/rs/xid"
+	"crypto/cipher"
+	"errors"
+	"io"
 )
 
 
@@ -11,6 +15,7 @@ import (
 // It will return an error if the system's secure random
 // number generator fails to function correctly, in which
 // case the caller should not continue.
+
 func GenerateRandomBytes(n int) ([]byte, error) {
     b := make([]byte, n)
     _, err := rand.Read(b)
@@ -31,4 +36,45 @@ func GenerateRandomString(s int) (string, error) {
 func Generateid() (string) {
     guid := xid.New()
     return guid.String()
+}
+func Encrypt(plaintext []byte, key *[32]byte) (ciphertext []byte, err error) {
+	block, err := aes.NewCipher(key[:])
+	if err != nil {
+		return nil, err
+	}
+
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return nil, err
+	}
+
+	nonce := make([]byte, gcm.NonceSize())
+	_, err = io.ReadFull(rand.Reader, nonce)
+	if err != nil {
+		return nil, err
+	}
+
+	return gcm.Seal(nonce, nonce, plaintext, nil), nil
+}
+
+func Decrypt(ciphertext []byte, key *[32]byte) (plaintext []byte, err error) {
+	block, err := aes.NewCipher(key[:])
+	if err != nil {
+		return nil, err
+	}
+
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return nil, err
+	}
+
+	if len(ciphertext) < gcm.NonceSize() {
+		return nil, errors.New("malformed ciphertext")
+	}
+
+	return gcm.Open(nil,
+		ciphertext[:gcm.NonceSize()],
+		ciphertext[gcm.NonceSize():],
+		nil,
+	)
 }
