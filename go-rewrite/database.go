730f21cdb44130c1cd7f6da638fc21954e87d51b
Anthony Bible <anthony@anthony.bible>
2021-06-17 06:08:28 -0600

    Reformatting

--- go-rewrite/database.go
+++ go-rewrite/database.go
@@ -6,25 +6,27 @@ import (
 	"fmt"
 )
 func Connect(){
-	dbHost := GetViperVariable("dbhost")
-	dbPass := GetViperVariable("dbpass")
-	dbUser := GetViperVariable("dbuser")
-	dbName := GetViperVariable("dbname")
-	dbPort := GetViperVariable("dbport")
-	// dbstring := dbuser + ":" + dbpass + "@tcp("  + dbhost  + ":" + dbport + ")/" + dbname
-	dbConnectionString := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", dbUser, dbPass, dbHost, dbPort, dbName)
-	// id:password@tcp(your-amazonaws-uri.com:3306)/dbname
+	dbhost := GetViperVariable("dbhost")
+	dbpass := GetViperVariable("dbpass")
+	dbuser := GetViperVariable("dbuser")
+	dbname := GetViperVariable("dbname")
+	dbport := GetViperVariable("dbport")
+	dbConnectionString := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", dbuser, dbpass, dbhost, dbport, dbname)
+
 	fmt.Println("this is the db string")
 	fmt.Print(dbConnectionString)
 	fmt.Sprintf("this is the dbstring: %s", dbConnectionString)
-	db, err := sql.Open("mysql", dbConnectionString)
+	fmt.Printf(dbConnectionString)
+	db, err := sql.Open("mysql", connectionString)
 	if err != nil {
-		fmt.Sprintf("this is the dbstring: %s", dbConnectionString)
+		// fmt.Sprintf("this is the dbstring: %s", dbConnectionString)
 		panic(err.Error()) // Just for example purpose. You should use proper error handling instead of panic
 	}
 	defer db.Close()
 	err = db.Ping()
     if err != nil {
       panic(err.Error()) // proper error handling instead of panic in your app
+   }else {
+	   fmt.Println("it wrks")
    }
 }
