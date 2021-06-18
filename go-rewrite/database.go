cfbd7e13ad973734ba287a979957f2f70a905c5e
Anthony Bible <anthony@anthony.bible>
2021-06-17 21:00:48 -0600

    Took out print statements

--- go-rewrite/database.go
+++ go-rewrite/database.go
@@ -13,23 +13,15 @@ func Connect(){
 	dbname := GetViperVariable("dbname")
 	// dbport := GetViperVariable("dbport")	
 	dbConnectionString := fmt.Sprintf("%s:%s@(%s)/%s", dbuser, dbpass, dbhost, dbname)
-	fmt.Println(net.LookupHost(dbhost))
-	fmt.Println(net.LookupHost("database-1.cyhholo0eone.us-west-2.rds.amazonaws.com"))
-	fmt.Println("this is the db string")
-	fmt.Print(dbConnectionString)
+
 	fmt.Sprintf("this is the dbstring: %s", dbConnectionString)
-	fmt.Printf(dbConnectionString)
 	db, err := sql.Open("mysql", dbConnectionString)
 	if err != nil {
-		fmt.Printf("this is the dbstring: %s", dbConnectionString)
 		panic(err.Error()) // Just for example purpose. You should use proper error handling instead of panic
 	}
 	defer db.Close()
 	err = db.Ping()
     if err != nil {
-		fmt.Printf("db ping: this is the dbstring: %s", dbConnectionString)
       panic(err.Error()) // proper error handling instead of panic in your app
-   }else {
-	   fmt.Println("it wrks")
    }
 }
