a019e12e2b47a965d684c5d8eec3425dc348e563
Anthony Bible <anthony@anthony.bible>
2021-09-08 20:45:42 -0600

    add grpc

--- app/encryption/encryption.go
+++ app/encryption/encryption.go
@@ -8,9 +8,13 @@ import (
 	"crypto/cipher"
 	"errors"
 	"io"
+	"net"
+	"log"
 	"fmt"
-	"password.exchange/message"
-	b "password.exchange/aws"
+	// "password.exchange/message"
+	// b "password.exchange/aws"
+	"password.exchange/protos/encryption"
+	"google.golang.org/grpc"
 )
 
 
@@ -84,20 +88,59 @@ func MessageDecrypt(ciphertext []byte, key *[32]byte) (plaintext []byte, err err
 }
 
 
-func main() {
-	msgEncrypted := &message.Message{
-	Email:   string(MessageEncrypt([]byte(c.PostForm("email")), encryptionbytes)),
-    FirstName: string(MessageEncrypt([]byte(c.PostForm("firstname")), encryptionbytes)),
-    OtherFirstName: string(MessageEncrypt([]byte(c.PostForm("other_firstname")), encryptionbytes)),
-    OtherLastName: string(MessageEncrypt([]byte(c.PostForm("other_lastname")), encryptionbytes)),
-    OtherEmail: string(MessageEncrypt([]byte(c.PostForm("other_email")), encryptionbytes)),
-    Content: string(MessageEncrypt([]byte(c.PostForm("content")), encryptionbytes)),
-    Uniqueid: guid.String(),
-  }
-  sess := b.BuildSession()
-  // queueurl, _ := b.GetQueueURL(sess, "arn:aws:sns:us-west-2:842805395457:my-test.fifo")
-  fmt.Println(len(urlEncodedString))
-  fmt.Println(urlEncodedString)
+// func main() {
+// 	msgEncrypted := &message.Message{
+// 	Email:   string(MessageEncrypt([]byte(c.PostForm("email")), encryptionbytes)),
+//     FirstName: string(MessageEncrypt([]byte(c.PostForm("firstname")), encryptionbytes)),
+//     OtherFirstName: string(MessageEncrypt([]byte(c.PostForm("other_firstname")), encryptionbytes)),
+//     OtherLastName: string(MessageEncrypt([]byte(c.PostForm("other_lastname")), encryptionbytes)),
+//     OtherEmail: string(MessageEncrypt([]byte(c.PostForm("other_email")), encryptionbytes)),
+//     Content: string(MessageEncrypt([]byte(c.PostForm("content")), encryptionbytes)),
+//     Uniqueid: guid.String(),
+//   }
+//   sess := b.BuildSession()
+//   // queueurl, _ := b.GetQueueURL(sess, "arn:aws:sns:us-west-2:842805395457:my-test.fifo")
+//   fmt.Println(len(urlEncodedString))
+//   fmt.Println(urlEncodedString)
 
-  b.SendSNS(sess, "arn:aws:sns:us-west-2:842805395457:my-test.fifo", msgEncrypted)
+//   b.SendSNS(sess, "arn:aws:sns:us-west-2:842805395457:my-test.fifo", msgEncrypted)
+// }
+
+
+type server struct {
+
+}
+
+func (*server) encryptMessage(ctx context.Context, request *encryption.Message) (*encryption.Message, error) {
+	// name := request.Name
+	// response := &hellopb.HelloResponse{
+	// 	Greeting: "Hello " + name,
+	// }
+	// return response, nil
+
+	msgEncrypted := &encryption.Message{
+		Email:   string(MessageEncrypt([]byte(c.PostForm("email")), encryptionbytes)),
+		FirstName: string(MessageEncrypt([]byte(c.PostForm("firstname")), encryptionbytes)),
+		OtherFirstName: string(MessageEncrypt([]byte(c.PostForm("other_firstname")), encryptionbytes)),
+		OtherLastName: string(MessageEncrypt([]byte(c.PostForm("other_lastname")), encryptionbytes)),
+		OtherEmail: string(MessageEncrypt([]byte(c.PostForm("other_email")), encryptionbytes)),
+		Content: string(MessageEncrypt([]byte(c.PostForm("content")), encryptionbytes)),
+		Uniqueid: guid.String(),
+	  }
+	  return msgEncrypted
+}
+
+
+func main() {
+	address := "0.0.0.0:50051"
+	lis, err := net.Listen("tcp", address)
+	if err != nil {
+		log.Fatalf("Error %v", err)
+	}
+	fmt.Printf("Server is listening on %v ...", address)
+
+	s := grpc.NewServer()
+	encryption.RegisterHelloServiceServer(s, &server{})
+
+	s.Serve(lis)
 }
