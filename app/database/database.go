8de925cf3b917a0af0906962d9ed859dea5133c4
Anthony Bible <anthony@anthony.bible>
2022-03-04 06:39:32 -0700

    seperate db to seperate service

--- app/database/database.go
+++ app/database/database.go
@@ -1,14 +1,19 @@
-package database
+package main
 
 import (
+	"context"
 	"database/sql"
 	"fmt"
+	"net"
+
+	db "github.com/Anthony-Bible/password-exchange/app/databasepb"
 
 	"github.com/Anthony-Bible/password-exchange/app/commons"
-	"github.com/Anthony-Bible/password-exchange/app/message"
-	"github.com/rs/zerolog/log"
-
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/rs/zerolog/log"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+	"google.golang.org/protobuf/types/known/emptypb"
 )
 
 func Connect() (db *sql.DB) {
@@ -38,6 +43,10 @@ func Connect() (db *sql.DB) {
 	return db
 }
 
+type server struct {
+	db.UnimplementedDbServiceServer
+}
+
 // func Select(id string	){
 // 	dbconn=Connect()
 // 	id := 1
@@ -55,23 +64,62 @@ func Connect() (db *sql.DB) {
 // }
 
 //Select Get the information based on the uuid from the url
-func Select(uuid string) (msgEncrypted message.Message) {
-	db := Connect()
-	err := db.QueryRow("select message,uniqueid from messages where uniqueid=?", uuid).Scan(&msgEncrypted.Content, &msgEncrypted.Uniqueid)
+// func (*server) DecryptMessage(ctx context.Context, request *pb.DecryptedMessageRequest) (*pb.DecryptedMessageResponse, error) {
+
+func (*server) Select(ctx context.Context, request *db.SelectRequest) (*db.SelectResponse, error) {
+	dbconnection := Connect()
+	response := db.SelectResponse{}
+	uuid := request.GetUuid()
+	err := dbconnection.QueryRow("select message,uniqueid from messages where uniqueid=?", uuid).Scan(&response.Content, &request.Uuid)
 
 	if err != nil {
 		log.Error().Err(err).Msg("Something went wrong with selecting from database")
+		return nil, err
 	}
-	return msgEncrypted
+	return &response, nil
 }
 
 //Insert encrypted information into database (this is base64 encoded)
-func Insert(msgEncrypted *message.Message) {
+func (*server) Insert(ctx context.Context, request *db.InsertRequest) (*emptypb.Empty, error) {
 	db := Connect()
 
-	_, err := db.Exec("INSERT INTO messages( message, uniqueid) VALUES(?,?)", msgEncrypted.Content, msgEncrypted.Uniqueid)
+	_, err := db.Exec("INSERT INTO messages( message, uniqueid) VALUES(?,?)", request.GetContent(), request.GetUuid())
 	if err != nil {
 		log.Error().Err(err).Msg("Something went wrong with Inserting into database")
 	}
+	e := &emptypb.Empty{}
+	return e, nil
 
+}
+
+func main() {
+	// Email:          []byte(ctx.PostForm("email"))
+	// FirstName:      []byte(ctx.PostForm("firstname"))
+	// OtherFirstName: []byte(ctx.PostForm("other_firstname"))
+	// OtherLastName:  []byte(ctx.PostForm("other_lastname"))
+	// OtherEmail:     []byte(ctx.PostForm("other_email"))
+	// Content:        []byte(ctx.PostForm("content"))
+
+	address := "0.0.0.0:50051"
+	lis, err := net.Listen("tcp", address)
+	if err != nil {
+		log.Fatal().Err(err).Msg("Problem with starting grpc server")
+	}
+	// plainMessage := &pb.PlainMessage{
+	// Email:          []byte(ctx.PostForm("email")),
+	// FirstName:      []byte(ctx.PostForm("firstname")),
+	// OtherFirstName: []byte(ctx.PostForm("other_firstname")),
+	// OtherLastName:  []byte(ctx.PostForm("other_lastname")),
+	// OtherEmail:     []byte(ctx.PostForm("other_email")),
+	// Content:        []byte(ctx.PostForm("content"))
+	// Url: siteHost + "decrypt/" + msgEncrypted.Uniqueid + "/" + string(encryptionstring[:]),
+
+	// }
+
+	s := grpc.NewServer()
+	db.RegisterDbServiceServer(s, &server{})
+	reflection.Register(s)
+	if err := s.Serve(lis); err != nil {
+		log.Fatal().Msgf("failed to serve: %v", err)
+	}
 }
