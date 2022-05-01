b2b1275f1b76d3ed7c2064209738b0d038d075f5
Anthony Bible <anthony@anthony.bible>
2022-05-01 14:33:30 -0600

    do lowercase phase

--- tools/bazel_stamp_vars.sh
+++ tools/bazel_stamp_vars.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 if [[ "${GITHUB_REF_TYPE}" =~ "tag" ]]; then
 echo "VERSION ${GITHUB_REF##*/}"
-echo "PHASE PROD"
+echo "PHASE prod"
 else
 echo "VERSION $(git rev-parse HEAD)-$(date +%s)"
-echo "PHASE DEV"
+echo "PHASE dev"
 fi
