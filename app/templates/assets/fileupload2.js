0ac766c1ec497edea6a063a5cac9a3568205f977
anthony bible <anthony@anthonybible.com>
2023-02-09 08:07:22 -0700

    New javascript working

--- app/templates/assets/fileupload2.js
+++ app/templates/assets/fileupload2.js
@@ -1,15 +1,19 @@
 // Function to upload a chunk of a file
-async function uploadChunk(chunk, fileId = null) {
+async function uploadChunk(chunk, chunkNumber, totalChunks, fileId = null, formData = null) {
   // API endpoint for uploading a chunk
   const apiUrl = 'https://dev.password.exchange';
 
   // Build the form data to be sent with the chunk
-  const formData = new FormData();
+  if (!formData) {
+    formData = new FormData();
+  }
+
   formData.append('file', chunk);
   if (fileId) {
     formData.append('fileId', fileId);
   }
-
+  formData.append('currentChunk', chunkNumber);
+  formData.append('totalChunks', totalChunks);
   // Make the API call to upload the chunk
   const response = await fetch(apiUrl, {
     method: 'POST',
@@ -18,10 +22,7 @@ async function uploadChunk(chunk, fileId = null) {
 
   // Return the file ID returned by the API
   const json = await response.json();
-  fileId = json.fileId;
-
-  // Return the file ID
-  return fileId;
+  return json;
 }
 
 // Function to upload a file in chunks
@@ -39,13 +40,19 @@ async function uploadFile(file) {
   // Calculate the start and end bytes for the chunk
 
   // Upload the first chunk and get the file ID
+  let json;
   if (numChunks > 0) {
     const start = 0;
     const end = Math.min(chunkSize, file.size);
     const chunk = file.slice(start, end);
-    const json = await uploadChunk(chunk);
-    fileId = json.fileId;
+    const formData = new FormData(document.querySelector('#contact-form'));
+    json = await uploadChunk(chunk, 1, numChunks, fileId, formData);
   }
+  if (numChunks === 1) {
+    return json.URL;
+  }
+  fileId = json.fileId;
+
   // check if fileid is empty
   if (fileId === null) {
     console.log('fileId is null');
@@ -57,14 +64,14 @@ async function uploadFile(file) {
     const start = i * chunkSize;
     const end = Math.min(start + chunkSize, file.size);
     const chunk = file.slice(start, end);
-    promises.push(uploadChunk(chunk, fileId));
+    promises.push(uploadChunk(chunk, i, numChunks, fileId));
   }
 
   // Wait for all the promises to resolve
   await Promise.all(promises);
 
   // Return the final file ID
-  return fileId;
+  return json.URL;
 }
 
 // Function to update the progress bar
@@ -75,15 +82,22 @@ document.querySelector('#contact-form').addEventListener('submit', async (event)
 
   // Get the file input element
   const fileInput = document.querySelector('#file-to-upload');
-
+  const submitButton = document.querySelector('#submitButton');
   const loadingBar = document.querySelector('#loading-bar');
+  const successArea = document.querySelector('#wasitasuccess');
   loadingBar.style.display = 'block';
-
+  submitButton.style.display = 'none';
   // Upload the file in chunks and update the progress bar
-  const fileId = await uploadFile(fileInput.files[0]);
+  const decryptUrl = await uploadFile(fileInput.files[0]);
 
   loadingBar.style.display = 'none';
-
+  submitButton.style.display = 'block';
+  // disable submit button
+  submitButton.disabled = true;
+  // turn successArea text green
+  successArea.style.color = 'green';
+  // display fileid on successarea
+  successArea.innerHTML = `Your file was uploaded successfully. <br> Your Decrypt URL is <a href="${decryptUrl}" target="_blank">${decryptUrl}</a>`;
   // Do something with the final file ID
-  console.log('File ID:', fileId);
+  console.log('Response:', decryptUrl);
 });
